a5b5c5826ec55d1cbace2a8877964e48a074ab66
bipin247 <bipin247@gmail.com>
2020-11-09 13:06:19 +0000

    test spring mvc Integration

---
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,39 @@
+package guru.springframework.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryTestIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    //@DirtiesContext
+    public void findByUom() {
+
+        assertEquals("Cup",
+              unitOfMeasureRepository.findByUom("Cup").get().getUom());
+    }
+
+    @Test
+    public void findByUomTeaspoon() {
+
+        assertEquals("Teaspoon",
+                unitOfMeasureRepository.findByUom("Teaspoon").get().getUom());
+    }
+}
